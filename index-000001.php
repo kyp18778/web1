dba423478983d69059b21f2f04ac49bbde88a0fe
kyp18778 <34851738+kyp18778@users.noreply.github.com>
2018-01-12 22:39:52 +0900

    Add files via upload

---
+++ index-000001.php
@@ -0,0 +1,261 @@
+<?
+
+$_zb_url="http://ypkim.cafe24.com/bbs/";
+
+$_zb_path= "/home/hosting_users/ypkim/www/bbs/";
+
+include $_zb_path."outlogin.php";
+
+?>
+
+<html>
+
+<head>
+
+<title>index</title>
+
+<style type="text/css">
+
+<!--
+
+
+
+a:link{
+
+text-decoration:none;
+
+font-family:"Arial", "Helvetica", "sans Serif";
+
+font-size:9pt;
+
+color:#444444;
+
+font-weight:bold;
+
+}
+
+
+
+a:visited{
+
+font-family:"Arial", "Helvetica", "sans Serif";
+
+font-size:9pt;
+
+color:#444444;
+
+text-decoration:none;
+
+}
+
+
+
+a:hover{
+
+font-family:"Arial", "Helvetica", "sans Serif";
+
+font-size:12pt;
+
+color:#3300cc;
+
+text-decoration:none;
+
+font-weight:bold;
+
+}
+
+
+
+
+
+//-->
+
+</style>
+
+
+
+<script Language="JavaScript">
+
+<!--
+
+function fun_open(){
+
+window.open("new_win2.html", "win_op", width=650, height=550)
+
+}
+
+//-->
+
+</script>
+
+
+
+<style>
+
+body{filter='progid:DXImageTransform.Microsoft.Gradient(gradientType=0,
+
+  StartColorStr=yellowgreen,  EndColorStr=#FFFFFF)'}    
+
+</style>
+
+
+
+<!-- 설명 "body 바로 위에 style 태그를 넣어 화면을 그라데이션으로 장식" 밑의 bg color는 위의 style 이 없을 때 반영됨
+
+//-->
+
+
+
+<meta http-equiv="Content-Type" content="text/html; charset=euc-kr">
+<meta name="generator" content="Namo WebEditor v5.0">
+</head>
+
+<body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+
+<!--
+<script LANGUAGE="javascript">
+window.open("http://ypkim.cafe24.com/popup/popup1.htm", "", "width=600, height=510, left=100, top=100");
+</script>
+//-->
+
+<table align=center width=1024 height=200 border=0 cellspasing=0 cellpadding=0>
+
+  <tr align=center>
+
+      <td>  <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://active.macromedia.com/flash4/cabs/swflash.cab#version=4,0,0,0" width="1024" height="200">
+            <param name="movie" value="top-01.swf">
+            <param name="play" value="true">
+            <param name="loop" value="true">
+            <param name="quality" value="high">
+            <embed src="top-01.swf" play="true" loop="true" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" width="1024" height="200"></embed>
+            </object></td>
+
+  </tr>
+
+</table>
+
+<table align=center width=800 height=25 border=0 cellspasing=0 cellpadding=0>
+
+  <tr align=center valign=bottom> 
+
+    <td width=81 bgcolor=#FF0000><a href=index.php>Home</a></td>
+
+    <td width=77 bgcolor=#99CCCC><a href=http://ypkim.cafe24.com/family.htm>가족소개</a></td>
+
+    <td width=77 bgcolor=yellow><a href=history.htm>전주김씨</a></td>
+
+    <td width="97" bgcolor=#FFFFCC><a href=firstname.htm>한국의성씨</a></td>
+
+    <td width="95" bgcolor=#CCFFFF><a href=alcohol.htm>음주운전</a></td>
+
+    <td width="91" bgcolor= #CCCCCC> <a href=http://ypkim.cafe24.com/bbs/zboard.php?id=bbs_01>자유게시판</a></td>      
+
+	<td width="87" bgcolor=yellow ><a href="http://ypkim.cafe24.com/bbs/zboard.php?id=bbs_02">가족대화방</a></td>
+
+    <td width=85 bgcolor=#CCFFFF><a href=http://ypkim.cafe24.com/bbs/zboard.php?id=photo target="_parent">사진보기</a></td>
+
+    <td width="90" bgcolor=#FFCCFF><a href=http://ypkim.cafe24.com/bbs/zboard.php?id=record>자료실</a></td>
+
+  </tr>
+
+</table>
+
+<table align=center width=800 height=453 border=0 cellspacing=0 cellpadding=0>
+
+  <tr align=center>
+
+     
+
+    <td  width=250 > <?print_outlogin("mywork",1,10)?> </td>
+
+    <td width=300>
+
+		<?print_bbs("nzeo/black_bbs","자유게시판","bbs_01",5,12)?>
+
+	</td>
+
+    <td width=250 valign="middle" > <p><a href="a/shutter.swf"><img src="images/family_01_17.jpg" width="170" height="126" border="0" align="bottom"></a></p></td>
+
+  </tr>    
+
+<tr align=center>
+
+     
+
+    <td width=250 height=226 valign="top"> <p> 
+
+        <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="105" height="59">
+
+          <param name="movie" value="countrybelive.swf">
+
+          
+
+          <param name="SCALE" value="noborder"><param name="QUALITY" value="low">
+
+          <embed src="countrybelive.swf" width="105" height="59" QUALITY="low" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" SCALE="noborder"></embed></object>
+
+      </p>
+
+      <p> 
+
+        <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="160" height="128">
+
+          <param name="movie" value="index(9999).swf">
+
+          <param name="quality" value="high">
+
+          <embed src="index(9999).swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="160" height="128"></embed></object>
+
+      </p>
+
+      </td>
+
+     <td width=300 ><?print_bbs("nzeo/black_bbs","가족대화방","bbs_02",5,12)?></td>
+
+     
+
+    <td width=250 valign="middle"> <p align="center"><IFRAME src=http://www.nalsee.com/nalsee/menu.html?id=kim18778&bid=9 width=130 height=120 frameborder=no scrolling=no></IFRAME>  </td>
+
+</tr>
+
+</table>
+
+<table width="550" align="center" border="0" cellpadding="2" cellspacing="0">
+
+  <tr> 
+
+    <td width="100" align="right" bgcolor=#FFFFFF> <font size="3" face="돋움체"><strong><em>KBS 
+
+      news</em></strong></font> </td>
+
+    <td width="450" bgcolor=  #FFFFFF align="center"> 
+
+      <iframe frameborder="0" src="http://ticker.kbs.co.kr/kbsnewsticker.html" width="340" height="18" scrolling="no"></iframe> 
+
+    </td>
+
+  </tr>
+
+</table><br>
+
+<table align=center width=800 height=25 border=0 cellspasing=0 cellpadding=0>
+
+  <tr align=center valign=bottom> 
+
+     
+
+      <td width=800 valign="middle" bgcolor=#ccccff><div align="center"><font size=2>Copyright 
+
+        &nbsp; &copy;&nbsp; 2004.01. y.p.km All Rights Reserved </font></div></td>
+
+     
+
+  </tr>
+
+</table>
+
+</body>
+
+
+
+</html>
